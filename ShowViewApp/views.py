d0b0c19fd9631a943ecdf44696f1adb47dd20ce2
treebug <trees.long@gmail.com>
2014-11-28 17:07:40 -0500

    Revert "errorpage"
    This reverts commit e1b0fce9d9581dfc5a68c129e2f6f215e22bc225.
    
    Conflicts:
    	ShowViewApp/__init__.pyc
    	ShowViewApp/admin.pyc
    	ShowViewApp/migrations/__init__.pyc
    	ShowViewApp/models.pyc

--- ShowViewApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response, redirect, render
-from django.template import RequestContext
-
-# Create your views here.
-def notFound(request):
-    return render_to_response('error.html',{'msg':'sorry can not found the page(404 ERROR)'})
