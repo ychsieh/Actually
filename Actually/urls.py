7af1f4c79fa8978ea611a45ce4de9b13ad669ca4
treebug <trees.long@gmail.com>
2014-12-05 02:41:40 -0500

    complete get past due json

--- Actually/urls.py
+++ Actually/urls.py
@@ -19,12 +19,12 @@ urlpatterns = patterns('',
     url(r'^viewproject/$', 'Auth_App.views.viewproject'),
     url(r'^ajaxtest/$', 'Auth_App.ajaxdata.test'),
     url(r'^jsontest/$', 'Auth_App.jsonservice.get_column_json'),
+    url(r'^get_past_due/$', 'Auth_App.jsonservice.get_pastdue_json'),
     url(r'^get_developer_json/$', 'Auth_App.jsonservice.get_developer_json'),
     url(r'^getpmjson/$', 'Auth_App.jsonservice.getpmjson'),
     url(r'^errorpage/$', 'Auth_App.errorhandler.show_error_msg'),
     url(r'^logout/$', 'Auth_App.views.logout'),
     url(r'^addproject/$', 'Auth_App.views.view_setup_project'),
-                   
  
 )
 
